6eb98a3f524f20b723e78454c17448374a57b812
Geotetra <n.chauffo.sco@gmail.com>
2016-04-22 14:14:53 +0200

    Modifié depuis la Machine

--- truc.cpp
+++ truc.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(void)
 {
   cout << "Prout Prout" << endl;
-  
+
   cout << "Modifié depuis GitHub" << endl;
+
+  cout << "Maintenat depuis la machine !"
   return(0);
 }
